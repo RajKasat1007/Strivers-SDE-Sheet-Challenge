8a83b75044a2d13389e88e62baa4fefa58db886f
RajKasat1007 <135512375+RajKasat1007@users.noreply.github.com>
2023-06-06 13:26:04 +0530

    Create Maximum_Subarray_Sum.cpp
    Solved maximum subarray sum

---
+++ Maximum_Subarray_Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long ans=0;
+    long long sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum<0){
+            sum=0;
+        }
+        ans=max(ans,sum);
+    }
+    return ans;
+}
