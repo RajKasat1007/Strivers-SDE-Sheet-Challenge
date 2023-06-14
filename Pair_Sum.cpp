ed3dd5106b9d9ffeebde81f4aafac378a62ed2b9
RajKasat1007 <135512375+RajKasat1007@users.noreply.github.com>
2023-06-14 11:14:59 +0530

    Create Pair_Sum.cpp
    Solved pair sum

---
+++ Pair_Sum.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   vector<vector<int>>ans;
+   for(int i=0;i<arr.size();i++){
+      for(int j=i+1;j<arr.size();j++){
+         if(arr[i]+arr[j]==s){
+            ans.push_back({min(arr[i],arr[j]),max(arr[i],arr[j])});
+         }
+      }
+   }
+   sort(ans.begin(),ans.end());
+   return ans;
+}
